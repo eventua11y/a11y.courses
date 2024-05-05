73ae75546c91356a6b1ff268cf4b90d2575f1284
Matt Obee <matt@obee.me>
2024-05-05 23:34:18 +0100

    chore: Update Astro configuration with Sanity integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  integrations: [sanity({
+    projectId: "2g5zqxo3",
+    dataset: "production",
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+  })]
 });
