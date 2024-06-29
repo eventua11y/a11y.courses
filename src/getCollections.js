9910f3b170a0382553789e05e398ff9146198cb7
Matt Obee <matt@obee.me>
2024-06-29 15:06:35 +0100

    feat: Refactor Course component and getCollections function

--- src/getCollections.js
+++ src/getCollections.js
@@ -1,14 +1,21 @@
 import { sanityClient } from "sanity:client";
 
-// Define an asynchronous function to get collections
 async function getCollections() {
   try {
-    const collections = await sanityClient.fetch(`*[_type == "collection" && !(_id in path("drafts.**"))]{
-      title,
-      description
+    const collections = await sanityClient.fetch(`*[_type == "collection" && count(courses[]) > 0 && !(_id in path("drafts.**"))]{
+      ...,
+      "courses": courses[]->{
+        ...,
+        "teacher": coalesce(teacher[]->{
+          ...
+        }, []),
+        "provider": coalesce(provider[]->{
+          ...
+        }, [])
+      }
     }`);
 
-    return collections; // Collections already have the desired structure
+    return collections; // Collections now include all fields for the collection, courses, teachers, and providers, with teacher and provider defaulting to [] if not present
   } catch (error) {
     console.error('Error fetching collections:', error);
     throw new Error(`Failed to fetch collections: ${error.message}`);
